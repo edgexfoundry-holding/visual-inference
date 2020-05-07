5145b9a4b7d05c1897df5550df3efc1496954ffc
Vaghesh Patel <vaghesh@gmail.com>
2020-05-06 19:17:41 -0700

    Updated build.sh to test latest release

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 
 VIDEO_ANALYTICS_SERVICE_REPO="https://github.com/intel/video-analytics-serving.git"
 
-sudo docker build ${VIDEO_ANALYTICS_SERVICE_REPO}#0.2.2-alpha -f Dockerfile.gst.base -t video_analytics_serving_gstreamer_base 
+#sudo docker build ${VIDEO_ANALYTICS_SERVICE_REPO}#0.2.2-alpha -f Dockerfile.gst.base -t video_analytics_serving_gstreamer_base 
 
-sudo docker build ${VIDEO_ANALYTICS_SERVICE_REPO}#0.2.2-alpha -f Dockerfile.gst -t video_analytics_serving_gstreamer 
+#sudo docker build ${VIDEO_ANALYTICS_SERVICE_REPO}#0.2.2-alpha -f Dockerfile.gst -t video_analytics_serving_gstreamer 
+
+sudo docker build ${VIDEO_ANALYTICS_SERVICE_REPO} -f Dockerfile.gst.base -t video_analytics_serving_gstreamer_base 
+
+sudo docker build ${VIDEO_ANALYTICS_SERVICE_REPO} -f Dockerfile.gst -t video_analytics_serving_gstreamer 
 
 sudo docker build -t video_analytics_serving_gstreamer_edgex:0.2.2 .
